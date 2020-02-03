73ecd267dc9ddcfc0e00ed1b3b8e9ef7a2d3a71e
Anton <dyadkin@greensight.ru>
2020-02-03 03:35:28 +0300

    обавлен en

--- langs/en/cont.php
+++ langs/en/cont.php
@@ -1,30 +1,123 @@
 <?php
 
 return [
-    'menu'  => [
-        1 => 'menu 1',
-        2 => 'menu 2',
-        3 => 'menu 3',
-        4 => 'menu 4',
+    'logo'      => '/html/assets/images/logos/white-text.png',
+    'menu'      => [
+        'services' => 'services',
+        'portfolio' => 'portfolio',
+        'order' => 'order',
+        'contact_us' => 'contact_us',
     ],
-    'logo'  => 'logo',
-    'title' => 'title in eng',
-    'slide' => [
-        'frst_slide' => [
-            'h1' => 'h1 slide 1',
-            'h2' => 'h2 slide 2'
+    'banner'    => [
+        'title' => 'title',
+        'sub'   => 'sub'
+    ],
+    'services'  => [
+        'title'   => 'title',
+        'sub'     => 'sub',
+        'service' => [
+            0 => [
+                'img'   => '/html/assets/images/portfolio/img4.jpg',
+                'text1' => 'title',
+                'text2' => 'sub',
+                'list' => [
+                    0 => 'text text text',
+                    1 => 'text text text',
+                    2 => 'text text text'
+                ],
+                'price'   => '$99',
+                'buy'     => 'заказать'
+            ],
+            1 => [
+                'img'   => '/html/assets/images/portfolio/img4.jpg',
+                'text1' => 'title',
+                'text2' => 'sub',
+                'list' => [
+                    0 => 'text text text',
+                    1 => 'text text text',
+                    2 => 'text text text'
+                ],
+                'price'   => '$99',
+                'buy'     => 'заказать'
+            ],
+            2 => [
+                'img'   => '/html/assets/images/portfolio/img4.jpg',
+                'text1' => 'title',
+                'text2' => 'sub',
+                'list' => [
+                    0 => 'text text text',
+                    1 => 'text text text',
+                    2 => 'text text text'
+                ],
+                'price'   => '$99',
+                'buy'     => 'buy'
+            ]
+        
         ],
-        'scnd_slide' => [
-            'h1' => 'h1 slide',
-            'h2' => 'h2 slide'
+    
+    ],
+    'portfolio'  => [
+        'title'   => 'title',
+        'sub'     => 'sub',
+        'work'    => [
+            0 => [
+                'img'   => '/html/assets/images/features/feature1/market.png',
+                'title' => 'title',
+                'sub'   => 'sub'
+            ],
+            1 => [
+                'img'   => '/html/assets/images/features/feature1/fruit.png',
+                'title' => 'title',
+                'sub'   => 'sub'
+            ],
+            2 => [
+                'img'   => '/html/assets/images/features/feature1/instant.png',
+                'title' => 'title',
+                'sub'   => 'sub'
+            ]
+        ]
+    
+    ],
+    'order'      => [
+        'title'  => 'title',
+        'img'    => '/html/assets/images/form-banners/banner2/banner-bg.jpg',
+        'form'   => [
+            0 => 'Name',
+            1 => 'lastname',
+            2 => 'tel',
+            3 => 'message',
+        ],
+        'booking' => 'order',
+        'message' => 'text',
+        'button'  => 'sbmt buy order'
+    ],
+    'contact_us' => [
+        'title'  => 'title',
+        'img'    => '/html/assets/images/contact/2.jpg',
+        'form'   => [
+            0 => 'Name',
+            1 => 'lastname',
+            2 => 'tel',
+            3 => 'message',
+        ],
+        'submit'  => 'submit',
+        'info'    => [
+            '/html/assets/images/contact/icon1.png' => [
+                'h6' => 'address',
+                'p'  => 'Perell 666 <br> Moscow',
+            ],
+            '/html/assets/images/contact/icon2.png' => [
+                'h6' => 'tel',
+                'p'  => '251 546 9442 <br> 630 446 8851',
+            ],
+            '/html/assets/images/contact/icon3.png' => [
+                'h6' => 'email',
+                'p' => 'info@wrappixel.com <br> 123@wrappixel.com',
+            ]
         ]
     ],
-    'article' => [
-        'frst_article' => [
-            'text' => 'text in eng 2',
-        ],
-        'scnd_article' => [
-            'text' => 'text in eng 2',
-        ]
+    'footer'   => [
+        'descr'  => 'text text text text',
+        'img'    => '/html/assets/images/logos/green-logo.png',
     ]
-];  
+];
